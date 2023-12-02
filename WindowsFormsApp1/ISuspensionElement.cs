145aa8a5c7f253b3558dcc516ed18c41fdacc3c0
slackstring <slackstring88@gmail.com>
2023-12-02 14:09:50 +0100

    InitialBuild

---
+++ WindowsFormsApp1/ISuspensionElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal interface ISuspensionElement
+    {
+        //properties
+        int Length { get; set; }
+        int Stroke { get; set; }
+        string SuspensionTyp { get; set; }
+        string Manufacturer { get; set; }
+        string Model { get; set; }
+        int ModelYear { get; set; }
+
+        
+
+        
+
+
+
+
+    }
+}
