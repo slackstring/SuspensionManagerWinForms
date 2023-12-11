74edc38659b39d25840408f9120401059cae21a3
slackstring <slackstring88@gmail.com>
2023-12-11 22:19:23 +0100

    added objectCreation of List<Setting> // started with Form SusElementMenu

--- WindowsFormsApp1/SuspensionElement.cs
+++ WindowsFormsApp1/SuspensionElement.cs
@@ -28,6 +28,7 @@ namespace WindowsFormsApp1
             Length = length;
             Stroke = stroke;
             SuspensionTyp = suspensionTyp;
+            Settings = new List<Setting>();         
         }
 
 
